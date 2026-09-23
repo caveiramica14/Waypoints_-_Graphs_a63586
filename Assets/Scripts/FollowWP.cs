5411ddf057c796e2f54ec2bfc5261065b816ed22
caveiramica14 <a63586@alunos.ipb.pt>
2026-09-23 15:03:01 +0100

    Waypoints passo 2

--- Assets/Scripts/FollowWP.cs
+++ Assets/Scripts/FollowWP.cs
@@ -6,6 +6,7 @@ public class FollowWP : MonoBehaviour
     int currentWP = 0;
 
     public float speed = 10.0f;
+    public float rotSpeed = 10.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,7 +25,11 @@ public class FollowWP : MonoBehaviour
             currentWP = 0;
         }
 
-        this.transform.LookAt(waypoints[currentWP].transform);
+        //this.transform.LookAt(waypoints[currentWP].transform);
+        
+        Quaternion lookatWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
+        
         this.transform.Translate(0, 0, speed * Time.deltaTime);
     }
 }
