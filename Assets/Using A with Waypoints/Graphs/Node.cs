d354923741b6c5d58c81d493b899e9865f83b93d
caveiramica14 <a63586@alunos.ipb.pt>
2026-09-24 23:12:13 +0100

    A Simple Graph API passo 3

--- Assets/Using A with Waypoints/Graphs/Node.cs
+++ Assets/Using A with Waypoints/Graphs/Node.cs
@@ -16,9 +16,6 @@ public class Node
     public Node(GameObject i)
     {
         id = i;
-        xPos = i.transform.position.x;
-        yPos = i.transform.position.y;
-        zPos = i.transform.position.z;
         path = null;
     }
 
